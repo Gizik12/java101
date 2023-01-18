e61b98a4484d560e3a21b983454e208f7f39cbbe
Mateusz Gizicki <76407981+Gizik12@users.noreply.github.com>
2023-01-19 00:34:44 +0100

    Update Zadanie2.java

--- w1/zadania/Gizik12/Zadanie2.java
+++ w1/zadania/Gizik12/Zadanie2.java
@@ -10,7 +10,7 @@ public class Zadanie2 {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        System.out.println(d);]
+        System.out.println(d);
         int e = -5;
         double g;
         double h;
