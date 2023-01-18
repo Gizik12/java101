f6d41e3fbb9f54a52dafe4387381d2238fa04ee6
Mateusz Gizicki <mgizicki77@gmail.com>
2023-01-19 00:32:24 +0100

    Rozwiązania zadań z zajęć 1

---
+++ w1/zadania/Gizik12/Zadanie2.java
@@ -0,0 +1,20 @@
+import static java.lang.Math.*;
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        double c;
+        double d;
+        c = pow(a,b);
+        d = sgrt(c);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);]
+        int e = -5;
+        double g;
+        double h;
+        g = abs(e);
+        h = 10.5001;
+        System.out.println(round(h));
+}
